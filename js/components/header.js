d54dbc515846dd35ff8b597b0ad10a2591e5e71f
strothj <jason@jasons.io>
2017-01-29 13:33:20 -0500

    Add new game function

--- js/components/header.js
+++ js/components/header.js
@@ -1,18 +1,29 @@
 var React = require('react');
+var connect = require('react-redux').connect;
 
+var actions = require('../actions/index');
 var Navbar = require('./navbar');
 
 var Header = React.createClass({
+    onNewGameClicked: function(event) {
+        event.preventDefault();
+        this.props.dispatch(actions.startNewGame());
+    },
+
+    onInfoClicked: function(event) {
+        alert('Game rules go here...');
+    },
+
     render: function() {
         return (
             <header className="header">
                 <Navbar
-                    onInfoClicked={this.props.onInfoClicked}
-                    onNewGameClicked={this.props.onNewGameClicked} />
+                    onInfoClicked={this.onInfoClicked}
+                    onNewGameClicked={this.onNewGameClicked} />
                 <h1 className="header__title">HOT or COLD</h1>
             </header>
         );
     }
 });
 
-module.exports = Header;
+module.exports = connect()(Header);
